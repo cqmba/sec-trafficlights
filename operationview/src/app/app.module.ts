87ba5e337a22a8c591c5bad00addd8ce8addd5a5
Stefan Pawlowski <sptemp@mailbox.org>
2020-01-02 23:12:05 +0100

    all mockups finished

--- operationview/src/app/app.module.ts
+++ operationview/src/app/app.module.ts
@@ -6,6 +6,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTableModule} from '@angular/material/table';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatChipsModule} from '@angular/material/chips';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
 import { DetailviewComponent } from './detailview/detailview.component';
 import { OverviewComponent } from './overview/overview.component';
 import { FooterComponent } from './footer/footer.component';
@@ -25,6 +31,12 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FlexLayoutModule,
     MatChipsModule,
     AppRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSlideToggleModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
