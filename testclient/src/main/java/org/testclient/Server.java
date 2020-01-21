04a7b69e46800612cfeaeb8dd0650a24fd3b7b24
Stefan Pawlowski <sptemp@mailbox.org>
2020-01-21 02:30:52 +0100

    Testclient can now connect with client credential workflow.

--- testclient/src/main/java/org/testclient/Server.java
+++ testclient/src/main/java/org/testclient/Server.java
@@ -5,10 +5,13 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.auth.User;
 import io.vertx.ext.auth.oauth2.OAuth2Auth;
 import io.vertx.ext.auth.oauth2.OAuth2ClientOptions;
 import io.vertx.ext.auth.oauth2.OAuth2FlowType;
+import io.vertx.ext.web.client.HttpRequest;
 import io.vertx.ext.web.client.HttpResponse;
 
 
@@ -21,34 +24,63 @@ public class Server extends AbstractVerticle {
     WebClient client = WebClient.create(vertx);
 
     server.requestHandler(req -> {
-    	
-    	OAuth2Auth oauth2 = OAuth2Auth.create(vertx, OAuth2FlowType.AUTH_CODE, new OAuth2ClientOptions()
+    	System.out.println("Hello req");
+    	OAuth2Auth oauth2 = OAuth2Auth.create(vertx, OAuth2FlowType.CLIENT, new OAuth2ClientOptions()
     			  .setClientID("vertx-test")
     			  .setClientSecret("98ba74b5-e0ff-4a23-b59e-b4e9c8658d09")
-    			  .setSite("http://localhost:8080/auth/")
-    			  .setTokenPath("/oauth/access_token")
-    			  .setAuthorizationPath("/oauth/authorize")
+    			  .setTokenPath("http://localhost:8080/auth/realms/vertx/protocol/openid-connect/token")
     			);
     	
-    	
-    	
-    	client
-    	  .get(8787, "localhost", "/protected/test")
-    	  .send(ar -> {
-    	    if (ar.succeeded()) {
+    	JsonObject tokenConfig = new JsonObject();
+    	System.out.println("start authenticate");
+    	oauth2.authenticate(tokenConfig, res -> {
+    		  if (res.failed()) {
+    		    System.err.println("Access Token Error: " + res.cause().getMessage());
+    		  } else {
+    		    // Get the access token object (the authorization code is given from the previous step).
+    			  User token = res.result();
+    			  System.out.println(token.principal().getString("access_token"));
+    			  System.out.println(token);
+    			  
+    			  client.get(8787, "localhost", "/protected/test")
+    			  .bearerTokenAuthentication(token.principal().getString("access_token"))
+    			  .send(ar -> {
+    				  	if (ar.succeeded()) {
     	      // Obtain response
-    	      HttpResponse<Buffer> response = ar.result();
-
-    	      System.out.println("Received response with status code" + response.statusCode());
-    	      System.out.println(response.bodyAsString());
-    	    } else {
-    	      System.out.println("Something went wrong " + ar.cause().getMessage());
-    	    }
-    	  });
+    				  		HttpResponse<Buffer> response = ar.result();
+   
+    				  		System.out.println("Received response with status code" + response.statusCode());
+    				  		System.out.println(response.bodyAsString());
+    				  	} else {
+    				  		System.out.println("Something went wrong " + ar.cause().getMessage());
+    				  	}
+    			  });
+   	    	
+  	    	
+  	      req.response().putHeader("content-type", "text/html")
+    	      .end("<h1>Hello vertx</h1>");
+    		  }
+    		});
     	
     	
-      req.response().putHeader("content-type", "text/html")
-      .end("<h1>Hello vertx</h1>");
+    	
+//    	client
+//    	  .get(8787, "localhost", "/protected/test")
+//    	  .send(ar -> {
+//    	    if (ar.succeeded()) {
+//    	      // Obtain response
+//    	      HttpResponse<Buffer> response = ar.result();
+//
+//    	      System.out.println("Received response with status code" + response.statusCode());
+//    	      System.out.println(response.bodyAsString());
+//    	    } else {
+//    	      System.out.println("Something went wrong " + ar.cause().getMessage());
+//    	    }
+//    	  });
+//    	
+//    	
+//      req.response().putHeader("content-type", "text/html")
+//      .end("<h1>Hello vertx</h1>");
     });
 
     server.listen(9090, ar -> {
